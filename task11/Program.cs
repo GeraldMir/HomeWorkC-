dca8b2a1dd9d3f7f6b0f5b6920dd38ee42531928
gerald <geramir91@gmail.com>
2022-07-12 21:00:08 +0300

    add task11

---
+++ task11/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = new Random().Next(100, 999);
+int numberSecond = number/100;
+int numberThree = number%10;
+int result = numberSecond*10 + numberThree;
+Console.WriteLine($"Наше случайное число={number}, исключая среднюю цифу = {result}");

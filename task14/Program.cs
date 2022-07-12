d5a95f03e1a3eb9879a102ed507020e7aab50713
gerald <geramir91@gmail.com>
2022-07-12 21:46:55 +0300

    add task 14

---
+++ task14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 14. Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+Console.WriteLine("Ввидите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+int delenie1= numberFirst%7;
+int delenie2= numberFirst%23;
+if (delenie1==0 && delenie2==0)
+{
+    Console.WriteLine("Число кратно, ответ ДА");
+}
+else
+{
+Console.WriteLine("Введеное число одновренно не является кратным 7 и 23, ответ НЕТ");
+}

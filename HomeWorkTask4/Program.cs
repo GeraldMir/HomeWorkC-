1fd42f93b70869134a1fe36beecb85ad51fd0919
gerald <geramir91@gmail.com>
2022-07-13 22:07:14 +0300

    add task 4, home work lesson one

---
+++ HomeWorkTask4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Ввидите первое число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввидите второе число: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввидите третье число: ");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+if (numberFirst == numberSecond && numberFirst == numberThird)
+{
+    Console.WriteLine("Числа одинаковые");
+}
+else
+{
+    int maxNumber = -1;
+    if (numberFirst > maxNumber)
+    {
+        maxNumber = numberFirst;
+    }
+    if (numberSecond > maxNumber)
+    {
+        maxNumber = numberSecond;
+    }
+    if (numberThird > maxNumber)
+    {
+        maxNumber = numberThird;
+    }
+    Console.WriteLine($"Максимальное число = {maxNumber}");
+}

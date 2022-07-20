cc83d9fa17cea452a2a8e09bd7f94d105a21b067
gerald <geramir91@gmail.com>
2022-07-20 19:56:29 +0300

    add classwork task and homework task №23

---
+++ ClassWorkLesson3Task18/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+// возможных координат точек в этой четверти (x и y).
+Console.Clear();
+Console.Write("Ввидите номер координатной четверти: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if(numb>4 || numb<1) Console.WriteLine("Недопустимое значение");
+if (numb == 1) Console.WriteLine("X > 0, Y > 0");
+    if (numb == 2) Console.WriteLine("X < 0, Y > 0");
+    if (numb == 3) Console.WriteLine("X < 0, Y < 0");
+    if (numb == 4) Console.WriteLine("X > 0, Y < 0");
---
+++ HomeWorkLesson3Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Ввидите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 1;
+Console.Write(evenNumber);
+while (N>evenNumber)
+{
+    evenNumber= evenNumber+1;
+    Console.Write("," + Math.Pow(evenNumber, 3));
+}

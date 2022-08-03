12ebd319c09337247e6a70ed184814dd8fb5e9df
gerald <geramir91@gmail.com>
2022-08-03 22:05:14 +0300

    add home work lesson 4 task 29

---
+++ HWLesson4Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент массива: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Clear();
+Console.Write("[" + string.Join(", ", arr) + "]");

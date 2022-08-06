b4c0a72494d88cc0089f4c9283ed2e9832765af3
gerald <geramir91@gmail.com>
2022-08-06 14:39:01 +0300

    add home work for lesson 5, task 34

---
+++ HWLesson5Task34/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+const int leftRange = 100;
+const int rightRange = 999;
+Console.WriteLine("Введите желаемый размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+   Random rand = new Random();
+   int[] arr = new int[size];
+
+   for (int i = 0; i < arr.Length; i++)
+   {
+      arr[i] = rand.Next(leftRange, rightRange + 1);
+   }
+   return arr;
+}
+
+int[] array =FillArrayWithRandomNumbers(size, leftRange, rightRange);
+int evenNumb = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2==0)
+    {
+        evenNumb+=1;
+    }
+}
+Console.WriteLine('[' + string.Join(", ", array) +"] -> " + evenNumb);

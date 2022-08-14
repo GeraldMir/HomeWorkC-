34afeee0cf160a486df713eedda61b36e7f9dc95
gerald <geramir91@gmail.com>
2022-08-14 22:22:17 +0300

    add home work for lesson 6, task №41 & №43

---
+++ HomeWorkLesson6Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел в одну линию (не используем Enter).
+// Числа разделены пробелами. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0 7 8 -2 -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.Write("Ввидите любые числа  через пробел ");
+string[] uzerArray= Console.ReadLine().Split();
+int result =0;
+for (int i = 0; i < uzerArray.Length; i++)
+{
+    if (Convert.ToInt32(uzerArray[i]) > 0)
+    {
+        result++;
+    }
+}
+Console.Write($"Введённые числа  {string.Join(", ", uzerArray)} -> {result} (количество чисел больше нуля)");
--- task11/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int number = new Random().Next(100, 999);
-int numberSecond = number/100;
-int numberThree = number%10;
-int result = numberSecond*10 + numberThree;
-Console.WriteLine($"Наше случайное число={number}, исключая среднюю цифу = {result}");
